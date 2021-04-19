0313b36e812426a04ffffe1f134a1608d92e8e99
파랑이 <parangee9706@gmail.com>
2021-04-19 21:35:24 +0900

    feat: button component

---
+++ components/Button.tsx
@@ -0,0 +1,17 @@
+import tw from 'twin.macro'
+import styled from 'styled-components'
+
+const Button = styled.div<{ color?: string; dark?: boolean }>`
+  ${tw`inline-block py-2 px-6 rounded-full cursor-pointer transition-all ease-in-out`}
+  background: ${({ color }) => color || '#7187D4'};
+  user-select: none;
+  color: ${({ dark }) => (dark || true ? '#fff' : '#000')};
+  &:hover {
+    transform: scale(1.1);
+  }
+  &:active {
+    transform: scale(1);
+  }
+`
+
+export default Button
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
+import Button from '../components/Button'
+
 export default function Home() {
   return (
-    <div className='no-underline text-black'>
-      {/*<div className='bg-gray-50'>*/}
-      {/*    <div className='flex gap-10 justify-center'>*/}
-      {/*        <h1 className='font-black text-xs leading-3 sm:text-6xl sm:leading-normal'>당신의 디스코드에<br />유니크한 재미를 더.</h1>*/}
-      {/*        <div className='sm:mt-10'>*/}
-      {/*            <FontAwesomeIcon  icon={["fab", "discord"]} className='text-9xl' color='#738ADB' />*/}
-      {/*        </div>*/}
-      {/*    </div>*/}
-      {/*</div>*/}
+    <div>
+      <Button>asdf</Button>
     </div>
   )
 }
