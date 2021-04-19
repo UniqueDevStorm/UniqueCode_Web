ea4ed079657185eda5f52b69475681ec4f014cac
파랑이 <parangee9706@gmail.com>
2021-04-19 22:49:00 +0900

    feat: re-initialize footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,51 +1,24 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
 
-export default function Footer() {
+const Container = styled.div`
+  background: #f4f4f6;
+  min-height: 11rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${tw`md:px-24 py-4 px-4 flex-col md:flex-row`}
+`
+
+const Footer = () => {
   return (
-    <div className='bg-gray-100'>
-      <div
-        className='container mx-auto h-44 w-full flex justify-between flex-col lg:flex-row'
-        style={{
-          alignItems: 'center',
-        }}
-      >
-        <div>
-          <img
-            src='/UniqueCode.png'
-            height={52}
-            width={52}
-            className='inline-block'
-          />
-          <h1 className='font-thin text-xl inline-block'>UniqueCode</h1>
-        </div>
-        <div className='text-center font-thin text-xl'>
-          <h1>
-            2021 &copy; UniqueCode All Right Reserved.
-            <br />
-            대표 : 송찬우 | 사업자등록번호 : 799-60-00477
-          </h1>
-        </div>
-        <div>
-          <a href='https://discord.gg/ARCdUzC' target='_blank'>
-            <FontAwesomeIcon
-              icon={['fab', 'discord']}
-              className='text-5xl'
-              color='#000000'
-            />
-          </a>
-          <a
-            href='https://github.com/UniqueCodeGit'
-            className='ml-5'
-            target='_blank'
-          >
-            <FontAwesomeIcon
-              icon={['fab', 'github']}
-              className='text-5xl'
-              color='#000000'
-            />
-          </a>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <div>LEFT</div>
+      <div>CENTER</div>
+      <div>RIGHT</div>
+    </Container>
   )
 }
+
+export default Footer
