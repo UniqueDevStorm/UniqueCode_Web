114e388e6a12107d29b81ac7ac82a8b41ee21767
파랑이 <parangee9706@gmail.com>
2021-04-20 15:52:14 +0900

    feat: project box roller

--- components/ProjectBoxRoller.tsx
+++ components/ProjectBoxRoller.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const ProjectBoxRoller: React.FC = ({ children }) => {
   if (!(children instanceof Array)) return
-  return (
-    <div>
-      {children.map((x, y) => (
-        <div key={y}>
-          {y}
-          {x}
-        </div>
-      ))}
-    </div>
-  )
+
+  const [current, setCurrent] = React.useState(0)
+
+  React.useEffect(() => {
+    const timer = setTimeout(
+      () => setCurrent(children.length === current + 1 ? 0 : current + 1),
+      4000
+    )
+    return () => clearTimeout(timer)
+  }, [current, setCurrent])
+
+  return <div>{children[current]}</div>
 }
 
 export default ProjectBoxRoller
