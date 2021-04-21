3490d135d9a0c7da2bbf2394f6ff252709bba7e8
UniqueDevStorm <storm@stormdev.club>
2021-04-21 11:44:53 +0900

    feat: change box structure

--- components/User.tsx
+++ components/User.tsx
@@ -16,7 +16,7 @@ const User: React.FC<UserProps> = ({
             <img
                 src={img}
                 style={{ borderRadius: '50px', boxShadow: '10px 10px 30px #74747429' }}
-                className='h-28 w-28 sm:h-40 sm:w-40'
+                className='h-24 w-24 sm:h-40 sm:w-40'
                 alt={Name}
             />
             <div className='block font-thin mt-5 ml-5 sm:mt-10 sm:ml-10'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,39 @@ import BananaMilk from '@assets/img/projects/BananaMilk.png'
 import Uniquebots from '@assets/img/projects/Uniquebots.png'
 import { NextSeo } from "next-seo";
 
+const Projects = [
+  {
+    name: "StrawberryDonut",
+    description: "나만의 스토리를 만들다.",
+    img: StrawberryDonut
+  },
+  {
+    name: "OliveToast",
+    description: "서버를 관리해줍니다.",
+    img: OliveToast
+  },
+  {
+    name: 'MiniBOT',
+    description: '커스텀 봇을 봇으로 제작하세요.',
+    img: MiniBOT
+  },
+  {
+    name: 'FreeAI',
+    description: '다기능 챗봇.',
+    img: FreeAI
+  },
+  {
+    name: 'BananaMilk',
+    description: '모두 함께 이야기를 재미있게.',
+    img: BananaMilk
+  },
+  {
+    name: 'Uniquebots',
+    description: '당신의 디스코드 봇을 홍보하세요.',
+    img: Uniquebots
+  }
+]
+
 export default function Home() {
   return (
     <div>
@@ -16,7 +49,7 @@ export default function Home() {
         title='UniqueCode'
         description='UniqueCode WebSite.'
       />
-      <div className='flex items-center justify-center gap-5'>
+        <div className='flex items-center justify-center gap-5'>
         <h1 className='text-2xl sm:text-7xl font-thin'>
           당신의 디스코드에
           <br />
@@ -30,32 +63,14 @@ export default function Home() {
       </div>
       <div className='flex justify-center mt-24'>
         <ProjectBoxRoller>
-          <ProjectBox
-            img={StrawberryDonut}
-            name='StrawberryDonut'
-            description='나만의 스토리를 만들다.'
-          />
-          <ProjectBox
-            img={OliveToast}
-            name='OliveToast'
-            description='서버를 관리해줍니다.'
-          />
-          <ProjectBox
-            img={MiniBOT}
-            name='MiniBOT'
-            description='커스텀 봇을 봇으로 제작하세요.'
-          />
-          <ProjectBox img={FreeAI} name='FreeAI' description='다기능 챗봇.' />
-          <ProjectBox
-            img={BananaMilk}
-            name='BananaMilk'
-            description='모두 함께 이야기를 재미있게.'
-          />
-          <ProjectBox
-            img={Uniquebots}
-            name='Uniquebots'
-            description='당신의 디스코드 봇을 홍보하세요.'
-          />
+          {Projects.map((x, y) => (
+              <ProjectBox
+                  img={x.img}
+                  name={x.name}
+                  description={x.description}
+                  key={y}
+              />
+          ))}
         </ProjectBoxRoller>
       </div>
     </div>
