ebc54360ffb776b0692d9ac27bdd981a9514cf88
UniqueDevStorm <storm@stormdev.club>
2021-04-21 14:02:40 +0900

    feat: Main Page Responsive

--- components/ProjectsBox.tsx
+++ components/ProjectsBox.tsx
@@ -26,11 +26,11 @@ const ProjectBox: React.FC<ProjectProps> = ({
 }) => {
     return (
         <div>
-            <Box className='flex items-center gap-10' style={{ borderRadius: '31px', width: '669px', height: '213px', boxShadow: '15px 15px 40px #ACACAC29' }}>
-                <img src={img} alt={name} style={{ borderRadius: '31px 0 0 31px' }} width={213} height={213} />
+            <Box className='flex items-center gap-4 sm:gap-10 w-80 h-24 sm:w-669 sm:h-213' style={{ borderRadius: '31px', boxShadow: '15px 15px 40px #ACACAC29' }}>
+                <img src={img} alt={name} style={{ borderRadius: '31px 0 0 31px' }} className='w-24 h-24 sm:w-213 sm:h-213' width={213} height={213} />
                 <div className='block font-thin'>
-                    <h1 className='sm:text-5xl'>{name}</h1>
-                    <h2 className='sm:text-2xl'>{description}</h2>
+                    <h1 className='text-xl sm:text-5xl'>{name}</h1>
+                    <h2 className='text-sm sm:text-2xl'>{description}</h2>
                 </div>
             </Box>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     extend: {
       gridTemplateRows: {
         '11': 'repeat(11, minmax(0, 1fr))'
+      },
+      spacing: {
+        '669': '41.813rem',
+        '213': '13.313rem'
+      },
+      height: {
+        '213': '13.313rem'
       }
     },
   },
